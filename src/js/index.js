9fb9b295c81fe40fc3000f435891442a28050cb9
PhPloveerPhP <marioperezmarrero@gmail.com>
2023-04-04 16:00:54 +0200

    Error while writting more than 2 lines

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,36 @@
 // DOM Elements
-const input = document.getElementById("user-input");
+let userinput = document.getElementById("user-input");
+let output = document.getElementById("output");
+// Variables
+let cont = 0;
 
-// Imput animation
-input.addEventListener("focus", function () {
-    input.style.animationPlayState = "running";
+
+// Event Listeners
+userinput.addEventListener("keydown", function (e) {
+    let div = document.createElement("div");
+    div.classList.add("flex");
+    if (e.key === "Enter") {
+        cont++;
+        div.innerHTML = `<span class="text-green-400">indexer:~$</span><input id=${cont} type="text" class="pl-2 text-gray-100 bg-gray-800 w-full outline-0" >`;
+        output.appendChild(div);
+        document.getElementById(cont).focus();
+        userinput.disabled = true;
+
+        let inputs = document.querySelectorAll("input");
+        inputs.forEach((input) => {
+            input.addEventListener("keydown", (e) => {
+                if (e.key === "Enter") {
+                    let div = document.createElement("div");
+                    div.classList.add("flex");
+                    if (e.key === "Enter") {
+                        cont++;
+                        div.innerHTML = `<span class="text-green-400">indexer:~$</span><input id=${cont} type="text" class="pl-2 text-gray-100 bg-gray-800 w-full outline-0" >`;
+                        output.appendChild(div);
+                        document.getElementById(cont).focus();
+                        userinput.disabled = true;
+                    }
+                }
+            });
+        });
+    }
 });
-
-input.addEventListener("blur", function () {
-    input.style.animationPlayState = "paused";
-});
-
-// Get Active ELement Id
-function onMouseUp(e) {
-    const activeTextarea = document.activeElement;
-    const selection = activeTextarea.value.substring(
-        activeTextarea.selectionStart,
-        activeTextarea.selectionEnd
-    );
-
-    console.log(selection);
-}
