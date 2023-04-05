a0655d426b7b63e2cd825b11779b73915ba070cb
PhPloveerPhP <marioperezmarrero@gmail.com>
2023-04-05 15:05:08 +0200

    Functional api (pending server function)

--- api/server.js
+++ api/server.js
@@ -2,69 +2,18 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const { google } = require('googleapis');
-const key = require('./service_account.json');
+const key = require('../private/key.json');
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-app.get('/', (req, res) => {
-    fs.appendFile('urls.txt', '\nhttps://www.example.com/', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
-
-    const jwtClient = new google.auth.JWT(
-        key.client_email,
-        null,
-        key.private_key,
-        ['https://www.googleapis.com/auth/indexing'],
-        null
-    );
-
-    const batch = fs
-        .readFileSync('urls.txt')
-        .toString()
-        .split('\n');
-
-    console.log(batch);
-
-    jwtClient.authorize(function (err, tokens) {
-        if (err) {
-            console.log(err);
-            return res.status(500).send(err);
-        }
-
-        const items = batch.map((line) => {
-            return {
-                'Content-Type': 'application/http',
-                'Content-ID': '',
-                body:
-                    'POST /v3/urlNotifications:publish HTTP/1.1\n' +
-                    'Content-Type: application/json\n\n' +
-                    JSON.stringify({
-                        url: line,
-                        type: 'URL_UPDATED',
-                    }),
-            };
-        });
-
-        const options = {
-            url: 'https://indexing.googleapis.com/batch',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'multipart/mixed',
-            },
-            auth: { bearer: tokens.access_token },
-            multipart: items,
-        };
-        request(options, (err, resp, body) => {
-            console.log(body);
-            if (err) {
-                return res.status(500).send(err);
-            }
-            return res.send(body);
-        });
+app.get('/api/index', (req, res) => {
+    res.send({
+        message: 'Hello World!'
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
