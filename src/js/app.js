402559c4e572ed326452cbc079d102d6736693b2
PhPloveerPhP <marioperezmarrero@gmail.com>
2023-04-06 08:33:33 +0200

    App js (Pending sending urls to server) (Pending index overflow to div)

--- src/js/app.js
+++ src/js/app.js
@@ -10,39 +10,50 @@ let clearPrompt = () => {
     $(input).trigger('focus');
 }
 
-// Fetching Data from API
 let indexURL = async () => {
-    console.log('indexing...');
-    const response = await fetch('/api/index');
+    const response = await fetch("http://localhost:3000/api/index");
     const jsonData = await response.json();
-    console.log(jsonData);
 
+    jsonData.split("\n").forEach((line) => {
+        let div = $('<div class="flex text-sm"></div>');
+        let input = $('<input type="text" class="w-full pl-2 text-gray-100 bg-gray-800 outline-0">');
+        div.append(input);
+        $('#output').append(div);
+        $(input).val(line);
+        $(input).attr('disabled', true);
+    });
 }
 
 
-// Line Prompt Events 
-$('#output').on('change', function (e) {
+// Line Prompt Events
+$("#output").on("change", function (e) {
     let actualTarget = e.target;
-    if (e.target.nodeName === 'INPUT') {
-        $(actualTarget).on('keyup', function (e) {
+    if (e.target.nodeName === "INPUT") {
+        $(actualTarget).on("keyup", async function (e) {
             if (e.keyCode === 13) {
-                if ($(actualTarget).val().startsWith('ind ')) {
-                    console.log('indexing...');
-                    indexURL();
-                } if ($(actualTarget).val() == 'clear' || $(actualTarget).val() == 'cls') {
+                if ($(actualTarget).val().startsWith("ind ")) {
+                    await indexURL();
+                }
+                if (
+                    $(actualTarget).val() == "clear" ||
+                    $(actualTarget).val() == "cls"
+                ) {
                     clearPrompt();
                 } else {
                     let div = $('<div class="flex"></div>');
                     let span = $('<span class="text-green-400">indexer:~$</span>');
-                    let input = $('<input type="text" class="w-full pl-2 text-gray-100 bg-gray-800 outline-0">');
+                    let input = $(
+                        '<input type="text" class="w-full pl-2 text-gray-100 bg-gray-800 outline-0">'
+                    );
                     div.append(span);
                     div.append(input);
-                    $('.flex').last().after(div);
-                    $(actualTarget).attr('disabled', true);
-                    $(input).trigger('focus');
+                    $(".flex").last().after(div);
+                    $(actualTarget).attr("disabled", true);
+                    $(input).trigger("focus");
                 }
             }
         });
     }
 });
+
 
