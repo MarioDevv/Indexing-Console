01954f4fb187d29b8f3199ca6f928c224947cb49
Mario <marioperezmarrero@gmail.com>
2023-04-06 20:25:34 +0100

    Upload function

--- src/js/app.js
+++ src/js/app.js
@@ -18,6 +18,9 @@ $("#output").on("change", function (e) {
         }
         if ($(actualTarget).val() == "whoami") {
           whoami();
+        }
+        if ($(actualTarget).val() == "upload") {
+          uploadCredentials();
         } else {
           $(actualTarget).attr("disabled", true);
           nextPrompt();
@@ -36,10 +39,10 @@ let indexURL = async (urls) => {
     },
     body: JSON.stringify({
       url: urls,
+      key: JSON.parse(localStorage.getItem("credentials")),
     }),
   });
   const jsonData = await response.json();
-
   jsonData.split("\n").forEach((line) => {
     createMessage(line);
   });
@@ -79,14 +82,14 @@ let clearPrompt = () => {
 // whoami Command
 let whoami = () => {
   let div = $("<div></div>");
-  let text = $(`<p class="text-gray-100">Hi, I'm a simple indexer made with NodeJS</p>
+  let text =
+    $(`<p class="text-gray-100">Hi, I'm a simple indexer made with NodeJS</p>
     <p>Joking, my name is <span class="text-green-400">Mario</span></p>
-    <p>And I'm a <span class="text-green-400">Full Stack Web App Developer</span> and a php lover</p>
+    <p>And I'm a <span class="text-green-400">Full Stack Web App Developer</span> and a PhpLover</p>
     <p>Check if its true :) --> <a href="https://github.com/PhPloveerPhP">https://github.com/PhPloveerPhP</a></p>`);
-  
-    div.append(text);
+
+  div.append(text);
   $("#output").append(div);
-  nextPrompt();
 };
 
 // PopOver Hover Event
@@ -96,15 +99,41 @@ $("#info").on("click", function (e) {
   $(lastInput).attr("disabled", true);
   let info = $("<div></div>");
   let tittle = $(
-    '<h1 class="font-bold text-gray-100"> --------- Commands --------- </h1>'
+    '<h1 class="text-gray-100"> --------- Commands --------- </h1>'
   );
   let text = $(
     `<p class="text-gray-100">ind [url] - Index a URL</p>
       <p class="text-gray-100">cls/clear - Clear the terminal</p>
-      <p class="text-gray-100">whoami - Its me :O!</p>`
+      <p class="text-gray-100">update - Upload Google credentials file</p>
+      <p class="text-gray-100">whoami - Its me :O!</p>` 
   );
   info.append(tittle);
   info.append(text);
   $("#output").append(info);
   nextPrompt();
+});
+
+// Upload Credentials command
+let uploadCredentials = () => {
+  let div = $("<div></div>");
+  let text = $(
+    `<p class="text-gray-100">Please, upload your credentials.json file</p>
+    <input  type="file" id="file" name="file" accept=".json">`
+  );
+  div.append(text);
+  $("#output").append(div);
+  nextPrompt();
+};
+
+// After Upload Credentials disable the input
+$("#output").on("change", "#file", function (e) {
+  let actualTarget = e.target;
+  $(actualTarget).attr("disabled", true);
+  let file = e.target.files[0];
+  let reader = new FileReader();
+  reader.readAsText(file);
+  reader.onload = function () {
+    let credentials = JSON.parse(reader.result);
+    localStorage.setItem("credentials", JSON.stringify(credentials));
+  };
 });
